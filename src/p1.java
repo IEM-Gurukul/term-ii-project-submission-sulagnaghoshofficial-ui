f7247ec2eca8f3cdc78e2723cb5f871200a75ca3
Sulagnaghoshofficial-ui <sulagnaghosh.official@gmail.com>
2026-03-25 22:58:37 +0530

    target user  section  added

---
+++ src/p1.java
@@ -0,0 +1,142 @@
+import java.util.*;
+
+class Book {
+    int id;
+    String title;
+    String author;
+    boolean isIssued;
+
+    Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+}
+
+class User {
+    int id;
+    String name;
+
+    User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class Library {
+    List<Book> books = new ArrayList<>();
+    List<User> users = new ArrayList<>();
+
+    void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added successfully!");
+    }
+
+    void viewBooks() {
+        for (Book b : books) {
+            System.out.println(b.id + " | " + b.title + " | " + b.author + " | Issued: " + b.isIssued);
+        }
+    }
+
+    void addUser(User user) {
+        users.add(user);
+        System.out.println("User added successfully!");
+    }
+
+    void issueBook(int bookId) {
+        for (Book b : books) {
+            if (b.id == bookId) {
+                if (!b.isIssued) {
+                    b.isIssued = true;
+                    System.out.println("Book issued successfully!");
+                } else {
+                    System.out.println("Book already issued!");
+                }
+                return;
+            }
+        }
+        System.out.println("Book not found!");
+    }
+
+    void returnBook(int bookId) {
+        for (Book b : books) {
+            if (b.id == bookId) {
+                if (b.isIssued) {
+                    b.isIssued = false;
+                    System.out.println("Book returned successfully!");
+                } else {
+                    System.out.println("Book was not issued!");
+                }
+                return;
+            }
+        }
+        System.out.println("Book not found!");
+    }
+}
+
+public class LibraryManagementSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Library lib = new Library();
+
+        while (true) {
+            System.out.println("\n--- Library Menu ---");
+            System.out.println("1. Add Book");
+            System.out.println("2. View Books");
+            System.out.println("3. Add User");
+            System.out.println("4. Issue Book");
+            System.out.println("5. Return Book");
+            System.out.println("6. Exit");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Book ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter Title: ");
+                    String title = sc.nextLine();
+                    System.out.print("Enter Author: ");
+                    String author = sc.nextLine();
+                    lib.addBook(new Book(id, title, author));
+                    break;
+
+                case 2:
+                    lib.viewBooks();
+                    break;
+
+                case 3:
+                    System.out.print("Enter User ID: ");
+                    int uid = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+                    lib.addUser(new User(uid, name));
+                    break;
+
+                case 4:
+                    System.out.print("Enter Book ID to issue: ");
+                    int issueId = sc.nextInt();
+                    lib.issueBook(issueId);
+                    break;
+
+                case 5:
+                    System.out.print("Enter Book ID to return: ");
+                    int returnId = sc.nextInt();
+                    lib.returnBook(returnId);
+                    break;
+
+                case 6:
+                    System.out.println("Exiting...");
+                    return;
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
+    }
+}
+// new line should be added soon
